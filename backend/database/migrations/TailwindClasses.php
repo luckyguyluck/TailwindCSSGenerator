2d5a90b995e026554212a1d5da222721b986a136
Luck <minkhant.mk13@icloud.com>
2025-04-13 02:54:25 +0630

    Database updated for color families.

--- backend/database/migrations/TailwindClasses.php
+++ backend/database/migrations/TailwindClasses.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('TailwindClasses', function (Blueprint $table) {
             $table->id();
             $table->string('classname');
             $table->string('prefix');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('classes');
+        Schema::dropIfExists('TailwindClasses');
     }
 };
--- backend/database/seeders/ClassesSeeder.php
+++ backend/database/seeders/ClassesSeeder.php
@@ -2,18 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TailwindClass;
 
-class ClassesSeeder extends Seeder
+class ClassSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $colors = ["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","rose","slate","gray","zinc","neutral","stone"];
-        $shades = [50,100,200,300,400,500,600,700,800,900,950];
-        $colortype = ["bg","text","outline","border","ring","placeholder","shadow","shadow-inner","shadow-outline"];
+        $colortypes = ["bg", "text", "outline", "border", "ring", "placeholder", "shadow", "shadow-inner", "shadow-outline"];
+        $colors = ["red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose", "slate", "gray", "zinc", "neutral", "stone"];
+        $shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+
+        foreach ($colortypes as $colortype) {
+            foreach ($colors as $color) {
+                foreach ($shades as $shade) {
+                    $classname = "{$colortype} {$color} {$shade}" ;
+                    $prefix = "{$colortype}-{$color}-{$shade}";
+                    $type = "{$colortype} color";
+
+                    TailwindClass::Create([
+                        'classname' => $classname,
+                        'prefix' => $prefix,
+                        'type' => $type,
+                    ]);
+                }
+            }
+        }
     }
 }
